69205ae514afe59815a6d751db37c48d2d6113de
zillurahman10 <zillur.hero40@gmail.com>
2023-07-30 19:11:26 +0600

    pc builder monitor add functionality done

---
+++ src/pages/addProduct/monitor.js
@@ -0,0 +1,37 @@
+import RootLayout from '@/components/Layout/RootLayout';
+import AddProductCard from '@/components/UI/AddProductCard';
+import React from 'react';
+
+const monitor = ({ components }) => {
+    const monitor = components?.data
+    return (
+        <>
+            <h1 className='text-2xl text-center m-12'>Visit the top monitor</h1>
+            <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 m-12'>
+                {
+                    monitor.map(product => <AddProductCard key={product?.id} product={product}></AddProductCard>)
+                }
+            </div>
+        </>
+    );
+};
+
+export default monitor;
+
+monitor.getLayout = function getLayout(page) {
+    return (
+        <RootLayout>
+            {page}
+        </RootLayout>
+    )
+}
+
+export const getStaticProps = async () => {
+    const res = await fetch("http://localhost:5000/monitor")
+    const data = await res.json()
+    return {
+        props: {
+            components: data
+        }
+    }
+}
